75aef3e68c0bd125a9ade869a26ee877127b4aad
Azad Salahli <azadsalahli@gmail.com>
2014-12-31 15:00:50 +0400

    Better interface

--- lightpush/main.py
+++ lightpush/main.py
@@ -1,5 +1,72 @@
+import configparser
+import logging
+import os
+import signal
+import sys
+
 from lightpush.server import Server
+from lightpush.utils import daemonize, FatalError
+
+
+def print_usage():
+    print('usage: lightpush command <config-file>'              )
+    print(''                                                    )
+    print('A lightweight push notification server for websites.')
+    print(''                                                    )
+    print('Commands:'                                           )
+    print('    start               starts the server'           )
+    # print('    restart             restarts the server'       )
+    print('    stop                stops the server'            )
+    print(''                                                    )
+    print('Arguments:'                                          )
+    print('    config-file         path to a configuration file')
+    print(''                                                    )
+
+
+def start(config):
+    settings = config['General']
+    host = settings['host']
+    port = int(settings['port'])
+    pidfile = settings['pidfile']
+    logfile = settings['logfile']
+    logging.basicConfig(filename=logfile, level=logging.DEBUG)
+
+    daemonize(pidfile, logfile)
+    Server(host, port).main()
+
+
+def stop(config):
+    settings = config['General']
+    pidfile = settings['pidfile']
+    f = open(pidfile, 'r')
+    pid = int(f.readline().strip())
+    os.kill(pid, signal.SIGTERM)
+    os.remove(pidfile)
 
 
 def main():
-    Server('', 8080).main()
+    try:
+        if len(sys.argv) != 3:
+            print_usage()
+            raise FatalError('Invalid arguments.')
+
+        command = sys.argv[1]
+        config_file = sys.argv[2]
+        config = configparser.ConfigParser()
+
+        try:
+            assert len(config.read(config_file)) == 1
+        except AssertionError:
+            raise FatalError('Configuration file could not be read.')
+        except configparser.Error:
+            raise FatalError('Invalid configuration file.')
+
+        if command == "start":
+            start(config)
+        elif command == "stop":
+            stop(config)
+        else:
+            print_usage()
+            raise FatalError('Invalid command: %s' % command)
+    except FatalError as e:
+        print('Error: %s' % e)
--- lightpush/utils.py
+++ lightpush/utils.py
@@ -1,5 +1,55 @@
+import os
+import sys
+
+
+class FatalError(Exception):
+    pass
+
+
 def create_packet(message):
     packet = bytearray(message)
     packet.insert(0, len(packet))
     packet.insert(0, 129)
     return packet
+
+
+def daemonize(pidfile, logfile):
+    """Daemonizes current process
+
+    This function executes the steps specified in 
+    Advanced Programming in the UNIX Environment, 3rd Edition
+    """
+    try:
+        # Clear file creation mask
+        os.umask(0)
+
+        # First fork()
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0)
+
+        # Create a new session
+        os.setsid()
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0)
+
+        pid = os.getpid()
+        f = open(pidfile, 'w')
+        f.write('%s\n' % pid)
+        f.close()
+
+        # Change current directory to root
+        os.chdir('/')
+
+        # Redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = open('/dev/null', 'r')
+        so = open(logfile, 'a+')
+        se = open(logfile, 'a+')
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+    except OSError as e:
+        raise FatalError('Failed to start lightpush daemon.')
